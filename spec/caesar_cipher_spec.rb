acdc6073fd3601c3cc942aa08bbd9d7c26069bfc
swifteye1111 <hasak.josh@gmail.com>
2022-12-14 11:06:48 +0100

    Fix expected result for tests 2 and 4

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -2,32 +2,32 @@ require './caesar_cipher'
 
 describe CaesarCipher do
   describe "#encrypt" do
-    it "works with lowercase letters without wrapping from z-a" do
+    it "encrypts lowercase letters without wrapping from z-a" do
       cipher = CaesarCipher.new
       expect(cipher.encrypt("goodtime", 6)).to eql("muujzosk")
     end
 
-    it "works with lowercase letters including wrapping from z-a" do
+    it "encrypts lowercase letters including wrapping from z-a" do
       cipher = CaesarCipher.new
-      expect(cipher.encrypt("goodtime", 7)).to eql("nvvkaptj")
+      expect(cipher.encrypt("goodtime", 7)).to eql("nvvkaptl")
     end
 
-    it "works with uppercase letters without wrapping from z-a" do
+    it "encrypts uppercase letters without wrapping from z-a" do
       cipher = CaesarCipher.new
       expect(cipher.encrypt("GOODTIME", 6)).to eql("MUUJZOSK")
     end
 
-    it "works with uppercase letters including wrapping from z-a" do
+    it "encrypts uppercase letters including wrapping from z-a" do
       cipher = CaesarCipher.new
-      expect(cipher.encrypt("GOODTIME", 7)).to eql("NVVKAPTJ")
+      expect(cipher.encrypt("GOODTIME", 7)).to eql("NVVKAPTL")
     end
 
-    it "works with both uppercase and lowercase letters including wrapping from z-a" do
+    it "encrypts both uppercase and lowercase letters including wrapping from z-a" do
       cipher = CaesarCipher.new
       expect(cipher.encrypt("What a good time", 6)).to eql("Cngz g muuj zosk")
     end
 
-    it "works with non-alphabet characters" do
+    it "encrypts non-alphabet characters" do
       cipher = CaesarCipher.new
       expect(cipher.encrypt("What a good time!", 6)).to eql("Cngz g muuj zosk!")
     end
